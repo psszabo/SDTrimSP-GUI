f7d9c8c235a70ca16c2c44193689ab26366ed035
psszabo <102395397+psszabo@users.noreply.github.com>
2022-04-03 18:08:15 -0700

    Delete setup directory

--- setup/setup_pyinstaller.py
+++
@@ -1,5 +0,0 @@
-# Since py2exe didn't work, pyinstaller was used
-# From inside the main folder, call
-#   pyinstaller main.py --icon=icons/tu_logo.ico --name=SDTrimSP-GUI --onefile --windowed --clean
-#
-# # REMOVE ANY "build" or "dist" folders before running this!
